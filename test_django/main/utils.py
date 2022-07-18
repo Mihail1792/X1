a943f8e7657b6f3669904ed2e94686085406cb30
mihail <mihail1792@list.ru>
2022-07-18 21:27:44 +0300

    add git

--- test_django/main/utils.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-def create_random_code():
-    """Генерирует случайный код"""
-    length = 6
-    char = string.ascii_letters + string.digits
-    return ''.join(random.choice(char) for x in range(length))
